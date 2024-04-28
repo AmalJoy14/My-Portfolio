41fffdfa3c4656644a1051d9b5e32c28fe454cfc
AmalJoy14 <amaljoy246@gmail.com>
2024-04-28 18:12:15 +0530

    added animations

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ cvBtn.addEventListener("click", () => {
 });
 //----------------------------------------------------------------
 
-//function to add drop effect to list
-function addEffect(number, delay, list) {
+//function to add effect to list
+function addEffect(number, delay, list,effect) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            list[number].classList.add("newDropEffect");
+            list[number].classList.add(effect);
             resolve("success");
         }, delay);
     });
@@ -17,8 +17,8 @@ function addEffect(number, delay, list) {
 //----------------------------------------------------------------
 
 //function to remove effect from list
-function removeEffect(number,list){
-    list[number].classList.remove("newDropEffect");
+function removeEffect(number,list,effect){
+    list[number].classList.remove(effect);
 }
 //-----------------------------------------------------------------
 
@@ -26,9 +26,9 @@ function removeEffect(number,list){
 const headerObjGrp = document.querySelectorAll(".HeaderObj");
 
 async function addDropEffectToHeader() {
-    await addEffect(0, 0,headerObjGrp);
+    await addEffect(0, 0,headerObjGrp,"newDropEffect");
     for (let i=1 ;i<=4 ;i++){
-        await addEffect(i, 300 ,headerObjGrp);
+        await addEffect(i, 300 ,headerObjGrp,"newDropEffect");
     }
 }
 addDropEffectToHeader();
@@ -41,13 +41,13 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach(async (entry) => {
         console.log(entry);
         if (entry.isIntersecting) {
-                await addEffect(0,0,skillIconGrp);
+                await addEffect(0,0,skillIconGrp,"newDropAliveEffect");
                 for (let i=1;i<9;i++){
-                    await addEffect(i, 300,skillIconGrp);
+                    await addEffect(i, 300,skillIconGrp,"newDropAliveEffect");
                 }
         } else {
                 for (let i=0;i<9;i++){
-                    removeEffect(i,skillIconGrp);
+                    removeEffect(i,skillIconGrp,"newDropAliveEffect");
                 }
         }
     });
@@ -56,3 +56,89 @@ const observer = new IntersectionObserver((entries) => {
 const skill = document.querySelector(".skills");
 observer.observe(skill); 
 //---------------------------------------------------------------------
+
+//Adding animation to titles
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add("newStampEffect");
+        }
+        else {
+            entry.target.classList.remove("newStampEffect");
+        }
+    });
+});
+
+const titlesGrp = document.querySelectorAll(".title");
+titlesGrp.forEach((el) => observer2.observe(el));
+//------------------------------------------------------------------------
+
+//Adding animation to contact
+const contactItemGrp = document.querySelectorAll(".contactItem");
+
+const observer3 = new IntersectionObserver((entries) => {
+    entries.forEach(async (entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+                await addEffect(0,0,contactItemGrp,"newDropEffect");
+                for (let i=1;i<5;i++){
+                    await addEffect(i, 300,contactItemGrp,"newDropEffect");
+                }
+        } else {
+                for (let i=0;i<5;i++){
+                    removeEffect(i,contactItemGrp,"newDropEffect");
+                }
+        }
+    });
+});
+
+const ContactMainBox = document.querySelector(".ContactMainBox");
+observer3.observe(ContactMainBox); 
+//-----------------------------------------------------------------------------
+
+//Adding animation to home
+const homeItemGrp = document.querySelectorAll(".homeItem, .Button1, .LinkedinIcon, .GithubIcon");
+
+const observer4 = new IntersectionObserver((entries) => {
+    entries.forEach(async (entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+                await addEffect(0,0,homeItemGrp,"newFade-inEffect");
+                for (let i=1;i<7;i++){
+                    await addEffect(i, 300,homeItemGrp,"newFade-inEffect");
+                }
+        } else {
+                for (let i=0;i<7;i++){
+                    removeEffect(i,homeItemGrp,"newFade-inEffect");
+                }
+        }
+    });
+});
+
+const profileContents = document.querySelector(".profileContent");
+observer4.observe(profileContents); 
+//-----------------------------------------------------------------------------
+
+//Adding animation to education
+const eduItemGrp = document.querySelectorAll(".EducationDuration, .Degree, .EducationLogo, .SchoolName, .grade");
+
+const observer5 = new IntersectionObserver((entries) => {
+    entries.forEach(async (entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+                await addEffect(0,0,eduItemGrp,"newDropEffect");
+                for (let i=1;i<15;i++){
+                    await addEffect(i,200,eduItemGrp,"newDropEffect");
+                }
+        } else {
+                for (let i=0;i<15;i++){
+                    removeEffect(i,eduItemGrp,"newDropEffect");
+                }
+        }
+    });
+});
+
+const EduBoxGrp = document.querySelector(".EducationBoxGroup");
+observer5.observe(EduBoxGrp); 
+//-----------------------------------------------------------------------------
